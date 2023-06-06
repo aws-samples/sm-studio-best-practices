15bc42449653cc6bceea98eba5a0b0c5ea3b0b59
Durga Sury <surydurg@amazon.com>
2023-06-06 15:24:07 -0600

    added overview to scripts

--- appendix/backup-restore-s3/backup-lcc.sh
+++ appendix/backup-restore-s3/backup-lcc.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+set -e
+# This script creates a snapshot of the user's root directory to a specific S3 bucket and the user profile name as the prefix. 
+# For instructions and use case, see Option 2: Back up from existing EFS using S3 and lifecycle configuration in the best practices documentation here: https://docs.aws.amazon.com/whitepapers/latest/sagemaker-studio-admin-best-practices/appendix.html#sagemaker-studio-domain-backup-and-recovery
+# Note: This should be created as a JupyterServer LCC configuration. If created as a KernelGateway LCC, the script will fail.
+# S3 sync creates a snapshot of the data when the LCC is run, and is not a real time backup. Run the sync.sh as a cron job if you wish to keep the s3 bucket synced in near real-time.
+# You can use the restore-lcc.sh to copy the S3 bucket contents to each user profile's home directory in a newly created domain.
 mkdir -p .backup
 cat << "EOF1" > /home/sagemaker-user/.backup/sync.sh
 #!/bin/bash
-# replace bucket name with your unique bucket
 BUCKET=sagemaker-domain-backup
 PREFIX=domain-backup
 cat > get-user-profile.py << EOF
--- appendix/backup-restore-s3/restore-lcc.sh
+++ appendix/backup-restore-s3/restore-lcc.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+set -e
+# This script copies files in a user-specific directory in S3 to the user's root directory.
+# If the user does not have a prefix in S3, the LCC skips the s3 sync and starts Studio for the user.
+# For instructions and use case, see Option 2: Back up from existing EFS using S3 and lifecycle configuration in the best practices documentation here: https://docs.aws.amazon.com/whitepapers/latest/sagemaker-studio-admin-best-practices/appendix.html#sagemaker-studio-domain-backup-and-recovery
+# Note: This should be created as a JupyterServer LCC configuration. If created as a KernelGateway LCC, the script will fail.
+# S3 sync creates a snapshot of the data when the LCC is run, and is not a real time backup. Run the sync.sh as a cron job if you wish to keep the s3 bucket synced in near real-time.
 mkdir -p .backup
 cat << "EOF1" > /home/sagemaker-user/.backup/restore.sh
 #!/bin/bash
